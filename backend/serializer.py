1c9524c534bd53e45cd20e3419b3ac29f3a8f136
Zakhar <yuzhackovza@gmail.com>
2024-05-31 04:17:45 +0500

    add serializers

--- backend/serializer.py
+++ backend/serializer.py
@@ -1,6 +1,26 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Location
+from .models import Location,SubDivision,Division,Group,Person
 class LocationsSerializer(ModelSerializer):
     class Meta:
         model = Location
+        fields ='__all__'
+
+class SubDivisionSerializer(ModelSerializer):
+    class Meta:
+        model = SubDivision
+        fields ='__all__'
+
+class DivisionSerializer(ModelSerializer):
+    class Meta:
+        model = Division
+        fields ='__all__'
+
+class GroupSerializer(ModelSerializer):
+    class Meta:
+        model = Group
+        fields ='__all__'
+
+class PersonSerializer(ModelSerializer):
+    class Meta:
+        model = Person
         fields ='__all__'
--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,15 @@
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
 from django.shortcuts import render, redirect
-from .models import Location, SubDivision, Division
-from .serializer import LocationsSerializer
+from .models import Location, SubDivision, Division,Group,Person
+from .serializer import LocationsSerializer,SubDivisionSerializer,DivisionSerializer,GroupSerializer,PersonSerializer
 
 
 
-def index(request):
-
-    return render(request, 'index.html',{'dop': LocationsSerializer(Location.objects.get(id = 1))})
+class index(APIView):
+    def get(self,request):
+        return Response(request, 'index.html',{'dop': (LocationsSerializer(Location.objects.all(), many = True).data,
+                                                                            SubDivisionSerializer(SubDivision.objects.all(), many = True).data,
+                                                                            DivisionSerializer(Division.objects.all(), many = True).data,
+                                                                            GroupSerializer(Group.objects.all(), many = True).data,
+                                                                            PersonSerializer(Person.objects.all(), many = True).data)})
