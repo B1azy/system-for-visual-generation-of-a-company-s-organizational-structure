bededddc164c589791fb1ba11fc9b610b3a8de8f
Zakhar <yuzhackovza@gmail.com>
2024-05-31 21:23:12 +0500

    fixed a bug in views.py

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
-from django.shortcuts import render, redirect
 from .models import Location, SubDivision, Division,Group,Person
 from .serializer import LocationsSerializer,SubDivisionSerializer,DivisionSerializer,GroupSerializer,PersonSerializer
 
@@ -8,8 +7,8 @@ from .serializer import LocationsSerializer,SubDivisionSerializer,DivisionSerial
 
 class index(APIView):
     def get(self,request):
-        return Response(request, 'index.html',{'dop': (LocationsSerializer(Location.objects.all(), many = True).data,
-                                                                            SubDivisionSerializer(SubDivision.objects.all(), many = True).data,
-                                                                            DivisionSerializer(Division.objects.all(), many = True).data,
-                                                                            GroupSerializer(Group.objects.all(), many = True).data,
-                                                                            PersonSerializer(Person.objects.all(), many = True).data)})
+        return Response((LocationsSerializer(Location.objects.all(), many = True).data,
+                         SubDivisionSerializer(SubDivision.objects.all(), many = True).data,
+                         DivisionSerializer(Division.objects.all(), many = True).data,
+                         GroupSerializer(Group.objects.all(), many = True).data,
+                         PersonSerializer(Person.objects.all(), many = True).data))
